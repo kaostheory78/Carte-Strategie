20a120142eea987e9b59ff4476df7251ae5f0ddb
Quentin Mercier <qmercier@hotmail.fr>
2014-12-21 01:05:09 +0100

    Ajout de fonctions_deplacements.c et .h

---
+++ Carte Strat V2.X/Fonctions_deplacements.c
@@ -0,0 +1,146 @@
+/******************************************************************************/
+/************** Carte polyvalente Botik  : DSPIC33FJ128MC804*******************/
+/******************************************************************************/
+/* Fichier 	: fonctions_deplacements.c
+ * Auteur  	: Quentin
+ * Revision	: 1.0
+ * Date		: 14 décembre 2014, 14:29
+ *******************************************************************************
+ *
+ *
+ ******************************************************************************/
+
+
+/******************************************************************************/
+/******************************** INCLUDES ************************************/
+/******************************************************************************/
+
+
+#include "system.h"
+
+void cibler (double x, double y)
+{
+    reinit_asserv();
+
+    x *= TICKS_PAR_MM;
+    y *= TICKS_PAR_MM;
+
+    TYPE_CONSIGNE = MM;
+
+    if ((y - Y.actuelle) != 0 || (x * - X.actuelle) != 0 )//&& (Y.consigne - Y.actuelle) != 0)
+    {
+        ORIENTATION.consigne =  atan2((y - Y.actuelle), (x - X.actuelle)) * ENTRAXE_TICKS/2 ;
+    }
+    
+    
+    
+
+  /*  if (ORIENTATION.consigne < -Pi * ENTRAXE_TICKS /2)
+        ORIENTATION.consigne += Pi * ENTRAXE_TICKS/2;
+    else if (ORIENTATION.consigne > Pi * ENTRAXE_TICKS/2)
+        ORIENTATION.consigne -= Pi * ENTRAXE_TICKS /2; */
+
+    FLAG_ASSERV.position = OFF;
+    FLAG_ASSERV.orientation = ON;
+    FLAG_ASSERV.vitesse = ON;
+
+    FLAG_ASSERV.etat_angle = EN_COURS;
+    FLAG_ASSERV.etat_distance = DISTANCE_ATTEINTE;
+    FLAG_ASSERV.immobilite = 0;
+
+    FLAG_ASSERV.fin_deplacement = DEBUT_DEPLACEMENT;
+
+    while(FLAG_ASSERV.fin_deplacement != FIN_DEPLACEMENT);
+    
+}
+
+void orienter (double angle)
+{
+
+    reinit_asserv();
+    
+    TYPE_CONSIGNE = MM;
+
+    ORIENTATION.consigne = (angle * Pi)/ 180 * (ENTRAXE_TICKS/2);
+
+    FLAG_ASSERV.position = OFF;
+    FLAG_ASSERV.orientation = ON;
+    FLAG_ASSERV.vitesse = ON;
+
+    FLAG_ASSERV.etat_angle = EN_COURS;
+    FLAG_ASSERV.etat_distance = DISTANCE_ATTEINTE;
+    FLAG_ASSERV.immobilite = 0;
+
+    FLAG_ASSERV.fin_deplacement = DEBUT_DEPLACEMENT;
+
+    while(FLAG_ASSERV.fin_deplacement != FIN_DEPLACEMENT);
+}
+
+
+void rejoindre (double x, double y)
+{
+    _rejoindre(x, y, ON);
+}
+
+void _rejoindre (double x, double y, char vitesse_fin_deplacement_nulle)
+{
+    reinit_asserv();
+
+    X.consigne = x * TICKS_PAR_MM;
+    Y.consigne = y * TICKS_PAR_MM;
+
+    TYPE_CONSIGNE = XY;
+
+    FLAG_ASSERV.position = ON;
+    FLAG_ASSERV.orientation = ON;
+    FLAG_ASSERV.vitesse = ON;
+
+    FLAG_ASSERV.etat_angle = EN_COURS;
+    FLAG_ASSERV.etat_distance = EN_COURS;
+    FLAG_ASSERV.vitesse_fin_nulle = vitesse_fin_deplacement_nulle;
+
+    FLAG_ASSERV.immobilite = 0;
+
+
+    FLAG_ASSERV.fin_deplacement = DEBUT_DEPLACEMENT;
+    while (FLAG_ASSERV.fin_deplacement != FIN_DEPLACEMENT);
+}
+
+void avancer_reculer (double distance)
+{
+    reinit_asserv();
+
+    TYPE_CONSIGNE = MM;
+
+    DISTANCE.consigne = distance * TICKS_PAR_MM;
+
+    FLAG_ASSERV.position = ON;
+    FLAG_ASSERV.orientation = OFF;
+    FLAG_ASSERV.vitesse = ON;
+
+    FLAG_ASSERV.etat_angle = ANGLE_ATTEINT;
+    FLAG_ASSERV.etat_distance = EN_COURS;
+    FLAG_ASSERV.immobilite = 0;
+
+    FLAG_ASSERV.fin_deplacement = DEBUT_DEPLACEMENT;
+    while (FLAG_ASSERV.fin_deplacement != FIN_DEPLACEMENT);
+}
+
+void passe_part (double x, double y)
+{
+    _rejoindre(x, y, OFF);
+}
+
+void passe_part2 (double x, double y)
+{
+    X.consigne = x * TICKS_PAR_MM;
+    Y.consigne = y * TICKS_PAR_MM;
+
+    FLAG_ASSERV.etat_angle = EN_COURS;
+    FLAG_ASSERV.etat_distance = EN_COURS;
+
+    FLAG_ASSERV.fin_deplacement = DEBUT_DEPLACEMENT;
+    while (FLAG_ASSERV.fin_deplacement != FIN_DEPLACEMENT);
+
+
+}
---
+++ Carte Strat V2.X/build/default/production/Fonctions_deplacements.o.d
@@ -0,0 +1,10 @@
+build/default/production/Fonctions_deplacements.o:  \
+ Fonctions_deplacements.c  \
+system.h  \
+codeurs.h  \
+Config_robots.h  \
+Uart.h  \
+ interruptions.h  \
+asserv.h  \
+pwm.h  \
+fonctions_deplacements.h 
